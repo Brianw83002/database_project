bb554529894d68e4583c8c38a42884afe221806d
brian <brianw83002@gmail.com>
2025-04-15 12:54:33 -0700

    used to check db connection

--- connect.php
+++ connect.php
@@ -1,15 +1,14 @@
 <?php
-//To run in cmd type 
+//To run in cmd type go to the folder where these files are located and enter: 
 // "php -S localhost:5500"
 // go to http://localhost:5500/connect.php
 
-
+//CHANGE THESE TO CHECK CONNECTION
 $username = "root";         // MySQL username
 $password = "";             // MySQL password
+$dbname = "users";          // The database you want to connect to (e.g., "users")
 
 $servername = "127.0.0.1";  // or "localhost"
-$dbname = "users";  // The database you want to connect to (e.g., "users")
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
